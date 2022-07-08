c7f2d8947cbbaac2dc4131c8cc9db9d69c0b6981
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-07-08 19:37:26 +0300

    add HomeTask #2 ¬(X V Y V Z) = ¬X A ¬Y A ¬Z

---
+++ Hometask002.py
@@ -0,0 +1,17 @@
+# 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = [0, 0, 0, 0, 1, 1, 1, 1]
+y = [0, 0, 1, 1, 0, 0, 1, 1]
+z = [0, 1, 0, 1, 0, 1, 0, 1]
+
+for i in range(8):
+    print(int(not(x[i] or y[i] or z[i]) == (not x[i] and not y[i] and not z[i])))
+
+#######################################################################################################################
+
+x = [False, False, False, False, True, True, True, True]
+y = [False, False, True, True, False, False, True, True]
+z = [False, True, False, True, False, True, False, True]
+
+for i in range(8):
+    print(not(x[i] or y[i] or z[i]) == (not x[i] and not y[i] and not z[i]))
